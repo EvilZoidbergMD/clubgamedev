4bf0f270430dc1207927c8defbf79ec25c36ca50
Kyle Engelken <EvilZoidbergMD@users.noreply.github.com>
2021-12-14 14:07:10 -0600

    Trying again on index.html

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const port = 3000;
 
 fs.readFile('./index.html', function (err, html) {
 
-    if (err) throw err;    
+    if (err) throw err;
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
     }).listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+        console.log(`Server running at http://${hostname}:${port}/`);
+    });
