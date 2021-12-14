0d9e7b9daa9ad4813dc30e8229d1a68c3088ecb9
Kyle Engelken <EvilZoidbergMD@users.noreply.github.com>
2021-12-14 16:12:21 -0600

    added installation page to server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const fs = require('fs');
 
@@ -5,8 +6,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('static'));
-app.get('/',(req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' });
-  fs.createReadStream('index.html').pipe(res)
+
+
+app.get('/', (req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    fs.createReadStream('index.html').pipe(res);
 });
+
+app.get('/installation.html', (req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    fs.createReadStream('installation.html').pipe(res);
+});
+
 app.listen(PORT, () => console.log(`Server listening in port ${PORT}`));
