56111b491910202a5ff9df9fc45eb8020c08e9ff
Kyle Engelken <EvilZoidbergMD@users.noreply.github.com>
2021-12-14 14:09:23 -0600

    One more time with index.js

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ const hostname = 'www.clubgamedev.com';
 const port = 3000;
 
 fs.readFile('./index.html', function (err, html) {
-
     if (err) throw err;
 
     http.createServer(function(request, response) {
         response.writeHeader(200, {"Content-Type": "text/html"});
         response.write(html);
         response.end();
-    }).listen(port, hostname, () => {
-        console.log(`Server running at http://${hostname}:${port}/`);
-    });
+    }).listen(port, hostname);
+});
