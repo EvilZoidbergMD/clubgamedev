1d7027f01130a862ef9dc717d825f5032917d5b5
Kyle Engelken <EvilZoidbergMD@users.noreply.github.com>
2021-12-14 15:46:10 -0600

    forgot to include fs in index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.get('/',(req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' });
   fs.createReadStream('index.html').pipe(res)
