57d11d0b092d7fa2b08decf575d4ed2e97c74fbf
Kyle Engelken <EvilZoidbergMD@users.noreply.github.com>
2021-12-15 09:00:06 -0600

    Added a public folder and moved static files into it

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //Static Files
-app.use(express.static('../'));
+app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'css'));
 app.use('/js', express.static(__dirname + 'js'));
-app.use('/img', express.static(__dirname + 'img'));
 
 
 app.set('views', './views');
